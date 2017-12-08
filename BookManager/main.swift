d5206f7ee3c54ae2acc5533ce13b2fc1da1716f6
김나영 <KNY@gimnayeongui-MacBook-Pro.local>
2017-12-08 17:09:40 +0900

    Initial Commit

---
+++ BookManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BookManager
+//
+//  Created by 김나영 on 2017. 12. 8..
+//  Copyright © 2017년 Ikarus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
